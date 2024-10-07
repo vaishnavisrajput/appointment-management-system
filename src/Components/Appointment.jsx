0e1647d83f9c34d42e3cb73b5dcfb618159e1294
Vaishnavi Singh Rajput <vaishnavirajput981@gmail.com>
2024-10-07 20:58:47 +0530

    first commit

--- src/Components/Appointment.jsx
+++ src/Components/Appointment.jsx
@@ -3,16 +3,27 @@ import { useState } from "react"
 function Appointment(){
     const[username, setUsername] = useState("");
     const[date, setDate] = useState("");
-    const[details, setDetails] = useState({});
+    const[appointments, setAppointments] = useState([]);
+    const[display, setDisplay] = useState(false);
     const handleClick = ()=> {
-        setDetails({username, date});
-        console.log(username, date);
+        if(username && date){
+            setAppointments([...appointments, { username, date }]);
+            console.log(appointments);
+            setDisplay(true);
+        }
         setUsername("");
         setDate("");
     }
+    // const handleDashboard = () =>{
+    //     setChange(false);
+    // }
     return(
+
+
+    
+
+        <> 
         
-        <>
             <div className="container">
                 <h1>Appointment Management System</h1>
                 {/* <h3>Book your appointment today!</h3> */}
@@ -22,23 +33,38 @@ function Appointment(){
                 <input type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)} />
                 <button className="btn" onClick={handleClick}>Book your Appointment</button>
             </div>
-            <div className="appoint-list">
+            {(display === false) ? (
+                <div className="quote">
+                    <h3>Book Your Appointment Today!</h3>
+                </div>
+            ) : (
+                <div className="appoint-list">
                 <div className="nav" >
-                    <label>S.No</label>
-                    <label>Name</label>
-                    <label>Date</label>
-                    <label>Edit/Delete</label>
+                    <p className="nav-label">S.No</p>
+                    <p className="nav-label">Name</p>
+                    <p className="nav-label">Date</p>
+                    <p className="nav-label">Edit</p>
+                    <p className="nav-label">Delete</p>
                 </div>
-            <div className="display" >
-                <p>S.No</p>
-                <p>Vaishnavi</p>
-                <p>09-08-2025</p>
+                {appointments.map((appointments) => (
+                    <div className="display"  >
+                <p className="p-tag">1</p>
+                <p className="p-tag">{appointments.username}</p>
+                <p className="p-tag">{appointments.date}</p>
                 <div className="edit-delete">
-                    <button className="btn">Edit</button>
-                    <button className="btn">Delete</button>
+                    <button onClick={handleEdit} className="edit-btn">Edit</button>
+                    <button className="delete-btn">Delete</button>
+                    
                 </div>
             </div>
+                ))}
+            
             </div>
+            )}
+            
+        
+        
+            
         </>
     )
 }
