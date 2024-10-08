b6ba0c28a8e29535f4136de7a64e54dd40447382
Vaishnavi Singh Rajput <vaishnavirajput981@gmail.com>
2024-10-08 17:02:24 +0530

    new functionality added

--- src/Components/Appointment.jsx
+++ src/Components/Appointment.jsx
@@ -1,72 +1,92 @@
-import React from "react"
-import { useState } from "react"
-function Appointment(){
-    const[username, setUsername] = useState("");
-    const[date, setDate] = useState("");
-    const[appointments, setAppointments] = useState([]);
-    const[display, setDisplay] = useState(false);
-    const handleClick = ()=> {
-        if(username && date){
-            setAppointments([...appointments, { username, date }]);
-            console.log(appointments);
-            setDisplay(true);
-        }
-        setUsername("");
-        setDate("");
+import React from "react";
+import { useState } from "react";
+function Appointment() {
+  const [username, setUsername] = useState("");
+  const [date, setDate] = useState("");
+  const [appointments, setAppointments] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleClick = () => {
+    if (username && date) {
+      if (editIndex === null) {
+        setAppointments([...appointments, { username, date }]);
+      } else {
+        const updatedAppointments = [...appointments];
+        updatedAppointments[editIndex] = { username, date };
+        setAppointments(updatedAppointments);
+        setEditIndex(null);
+      }
     }
-    // const handleDashboard = () =>{
-    //     setChange(false);
-    // }
-    return(
+    setUsername("");
+    setDate("");
+  };
+  const handleEdit = (index) => {
+    const editedValue = appointments[index];
+    setUsername(editedValue.username);
+    setDate(editedValue.date);
+    setEditIndex(index);
+  };
+
+  const handleDelete = (index) => {
+    const filteredList = appointments.filter((_, i) => i!== index);
+    setAppointments(filteredList);
+  }
 
 
-    
+  return (
+    <>
+      <div className="container">
+        <h1>Appointment Management System</h1>
+        <p>Enter your Full Name:</p>
+        <input
+          type="username"
+          name="fullName"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Enter your name"
+        />
+        <p>Date of Appointment:</p>
+        <input
+          type="date"
+          name="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
 
-        <> 
-        
-            <div className="container">
-                <h1>Appointment Management System</h1>
-                {/* <h3>Book your appointment today!</h3> */}
-                <p>Enter your Full Name:</p>
-                <input type="username" name="fullName" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your name"  />
-                <p>Date of Appointment:</p>
-                <input type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)} />
-                <button className="btn" onClick={handleClick}>Book your Appointment</button>
+        <button className="btn" onClick={handleClick}>
+          {editIndex === null ? "Book your Appointment" : "Save Changes"}
+        </button>
+      </div>
+      {appointments.length === 0 ? (
+        <div className="quote">
+          <h3>Book Your Appointment Today!</h3>
+        </div>
+      ) : (
+        <div className="appoint-list">
+          <div className="nav">
+            <p className="nav-label">S.No</p>
+            <p className="nav-label">Name</p>
+            <p className="nav-label">Date</p>
+            <p className="nav-label">Edit</p>
+            <p className="nav-label">Delete</p>
+          </div>
+          {appointments.map((appointments, index) => (
+            <div className="display" key={index}>
+              <p className="p-tag">{index + 1}</p>
+              <p className="p-tag">{appointments.username}</p>
+              <p className="p-tag">{appointments.date}</p>
+              <div className="edit-delete">
+                <button onClick={() => handleEdit(index)} className="edit-btn">
+                  Edit
+                </button>
+                <button onClick={() => handleDelete(index)} className="delete-btn">Delete</button>
+              </div>
             </div>
-            {(display === false) ? (
-                <div className="quote">
-                    <h3>Book Your Appointment Today!</h3>
-                </div>
-            ) : (
-                <div className="appoint-list">
-                <div className="nav" >
-                    <p className="nav-label">S.No</p>
-                    <p className="nav-label">Name</p>
-                    <p className="nav-label">Date</p>
-                    <p className="nav-label">Edit</p>
-                    <p className="nav-label">Delete</p>
-                </div>
-                {appointments.map((appointments) => (
-                    <div className="display"  >
-                <p className="p-tag">1</p>
-                <p className="p-tag">{appointments.username}</p>
-                <p className="p-tag">{appointments.date}</p>
-                <div className="edit-delete">
-                    <button onClick={handleEdit} className="edit-btn">Edit</button>
-                    <button className="delete-btn">Delete</button>
-                    
-                </div>
-            </div>
-                ))}
-            
-            </div>
-            )}
-            
-        
-        
-            
-        </>
-    )
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Appointment
+export default Appointment;
